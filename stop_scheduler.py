22284e97b8afe9c9b7abc07e0cffb0726f69bd5b
Thomas Wielatt <wielattt@mindpractise.com>
2023-07-12 21:13:06 +0200

    Bugfix: replaced the py file contents with the old py statements

--- stop_scheduler.py
+++ stop_scheduler.py
@@ -1,3 +1,22 @@
-#!/usr/bin/bash
+from os import path, remove, system
+import sys
+from modules.configuration import Configuration as config
 
-/usr/bin/python $(dirname "$0")/scheduler.py
+APP_FOLDER = sys.path[0]
+LOCK_FILEPATH = config.instance().getLockFilePath("scheduler")
+DIASHOW_APP_NAME = config.instance().getString("general", "diashow_app_name")
+
+
+def stopDiashow():
+    cmd = "kill $(pidof /usr/bin/" + DIASHOW_APP_NAME + ")"
+    system(cmd)
+
+def unlock():
+    try:
+        remove(LOCK_FILEPATH)
+    except Exception as e:
+        #print("Exception occurred while unlocking: " + str(e))
+        pass
+
+stopDiashow()
+unlock()
