b6f10335f59001f7ac0b42b15368321c44ad8c31
Thomas Wielatt <wielattt@mindpractise.com>
2023-07-12 21:03:55 +0200

    Bugfix: replaced python contents of bash file

--- scheduler.sh
+++ scheduler.sh
@@ -1,20 +1,4 @@
+#!/usr/bin/bash
 
+/usr/bin/python $(dirname "$0")/scheduler.py
 
-from os import path, remove, system
-import sys
-from modules.configuration import Configuration as config
-from modules import locking
-
-APP_FOLDER = sys.path[0]
-LOCK_FILEPATH = config.instance().getLockFilePath("scheduler")
-DIASHOW_APP_NAME = config.instance().getString("general", "diashow_app_name")
-
-
-def stopDiashow():
-    cmd = "kill $(pidof /usr/bin/" + DIASHOW_APP_NAME + ")"
-    system(cmd)
-
-print ("<<<<<<<<<<<<< stopDiashow started >>>>>>>>>>>>>>")
-stopDiashow()
-locking.unlock(LOCK_FILEPATH)
-print ("<<<<<<<<<<<<< stopDiashow finished >>>>>>>>>>>>>>")
